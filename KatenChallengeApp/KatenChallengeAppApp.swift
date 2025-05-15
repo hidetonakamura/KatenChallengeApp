c2fc6054c85afca11a85c99f3cff88f5b5d40cf6
ひでと <hideto.nakamura.0521@gmail.com>
2025-05-15 09:33:03 +0900

    Initial Commit

---
+++ KatenChallengeApp/KatenChallengeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KatenChallengeAppApp.swift
+//  KatenChallengeApp
+//
+//  Created by 仲村日出人 on 2025/05/15.
+//
+
+import SwiftUI
+
+@main
+struct KatenChallengeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KatenChallengeAppTests/KatenChallengeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  KatenChallengeAppTests.swift
+//  KatenChallengeAppTests
+//
+//  Created by 仲村日出人 on 2025/05/15.
+//
+
+import Testing
+@testable import KatenChallengeApp
+
+struct KatenChallengeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
